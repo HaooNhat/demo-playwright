efb34caa10b30306b642cd0d729b74a16a568d74
HaoNhat <haonhatht@gmail.com>
2024-10-28 14:45:39 +0700

    check test

--- tests/shop.spec.js
+++ tests/shop.spec.js
@@ -1,2 +1,7 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test');
 
+test('First test', async ({ page }) => {
+    await page.goto('https://practicesoftwaretesting.com/');
+
+    await expect(page).toHaveTitle('Practice Software Testing - Toolshop - v5.0');
+});
